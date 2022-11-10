17ca756acd0c09dbbc28a695fd04c28297b9cacc
faizan <sfh8881212@gmail.com>
2022-11-10 08:02:48 -0800

    all file committed

---
+++ ajaxgetting2022/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ajaxgetting2022.Models;
+namespace ajaxgetting2022.Controllers
+{
+    public class HomeController : Controller
+    {
+        ajaxDBEntities db = new ajaxDBEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddBatch()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddBatch(batchtb tb)
+        {
+            try
+            {
+                db.batchtbs.Add(tb);
+                db.SaveChanges();
+                ViewBag.msg = "Data Save";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.error = ex.ToString();
+            }
+            return View();
+        }
+
+        public ActionResult AddStudent()
+        {
+            ViewBag.batchID = new SelectList(db.batchtbs, "BatchID", "BatchName");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddStudent(studenttb st)
+        {
+            try
+            {
+                ViewBag.batchID = new SelectList(db.batchtbs, "BatchID", "BatchName");
+                db.studenttbs.Add(st);
+                db.SaveChanges();
+                ViewBag.msg = "Data Save";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.error = ex.ToString();
+            }
+            return View();
+        }
+
+        public ActionResult StudentReport()
+        {
+            var getbatch = db.batchtbs.ToList();
+            return View(getbatch);
+        }
+
+        public PartialViewResult ShowStudentReport(int id)
+        {
+            var gets = db.studenttbs.Where(x => x.BatchIDKF == id).ToList();
+            return PartialView(gets);
+        }
+
+    }
+}
---
+++ ajaxgetting2022/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ajaxgetting2022.MvcApplication" Language="C#" %>
